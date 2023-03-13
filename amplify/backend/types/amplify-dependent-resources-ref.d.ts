a329abe5566c4b20c0a0c748ed41fb14de7b3cd8
Kobbyjust <sagarinokoe1@gmail.com>
2023-03-13 06:36:03 +0000

    Revision update 2

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "reactamplifyapp1imagestorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
